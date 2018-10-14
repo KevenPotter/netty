1d2415b30f2d7e89702561ea312bed4689807c9e
KevenPotter <keven_potter@163.com>
2018-10-14 23:38:41 +0800

    15_Protobuf集成Netty与多协议消息传递

---
+++ src/main/java/cn/goldenet/netty/sixthexample/TestClientInitializer.java
@@ -0,0 +1,21 @@
+package cn.goldenet.netty.sixthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+public class TestClientInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("protobufVarint32FrameDecoder", new ProtobufVarint32FrameDecoder());
+        pipeline.addLast("protobufDecoder", new ProtobufDecoder(MyDataInfo.Person.getDefaultInstance()));
+        pipeline.addLast("protobufVarint32LengthFieldPrepender", new ProtobufVarint32LengthFieldPrepender());
+        pipeline.addLast("protobufEncoder", new ProtobufEncoder());
+        pipeline.addLast("testClientHandler", new TestClientHandler());
+    }
+}
