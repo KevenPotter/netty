5cadcfcace55deef812393531911fea00e39f72e
KevenPotter <Keven_Potter@163.com>
2018-11-20 23:00:03 +0800

    36_文件通道用法详解

---
+++ src/main/java/cn/goldenet/nio/NioTest4.java
@@ -0,0 +1,29 @@
+package cn.goldenet.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest4 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("input.txt");
+        FileOutputStream outputStream = new FileOutputStream("out.txt");
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(512);
+        while (true) {
+            buffer.clear();
+            int read = inputChannel.read(buffer);
+            System.out.println("read: " + read);
+            if (-1 == read) {
+                break;
+            }
+            buffer.flip();
+            outputChannel.write(buffer);
+        }
+        inputChannel.close();
+        outputChannel.close();
+    }
+}
